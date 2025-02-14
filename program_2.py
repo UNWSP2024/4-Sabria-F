6fce5a162898f51b612f023400f1726cab835f33
Sabria-F <sjfafach@students.unwsp.edu>
2025-02-14 00:40:53 -0600

    Program2MovieTixSabriaFafach

--- program_2.py
+++ program_2.py
@@ -1,14 +1,12 @@
-# Program #2: Movie Tix
-# Write a program that has the user input various movie names and how many 
-# tickets are desired for each movie.  
-# At the end of the program it prints out the total number of tickets desired by the user.  
-# Use either a "for loop" or "while loop" to accomplish this.
-
-def main():
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+#By: Sabria Fafach
+#Date: Febuary, 2 2025
+#Title: program_2.py
 
 
-if __name__ == '__main__':
-    main()
+movie_inp=input("Do you want to get tickets for a movie(s)? (Enter y for yes):")
+tot_tix=0
+while movie_inp=="y":
+    movie=input("Enter the name of a movie you want to get tickets for:")
+    tot_tix=int(input(f"Enter the number of tickets you want for {movie}:"))+tot_tix
+    movie_inp=input("Do you want to get tickets for anoter movie? (Enter y for yes):")
+print(f"The total of all your movie tickets is {tot_tix} tickets.")
